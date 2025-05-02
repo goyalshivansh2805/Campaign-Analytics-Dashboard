dba17ed75938c76e715211ebc741baf889b01a62
Shivansh Goyal <goyal.shivansh2805@gmail.com>
2025-05-02 08:12:01 +0530

    Added ENV file for API URL

--- frontend/app/lib/api.ts
+++ frontend/app/lib/api.ts
@@ -1,7 +1,7 @@
 import { Campaign } from "../types";
 
 export async function fetchCampaigns(): Promise<Campaign[]> {
-  const res = await fetch("http://localhost:8000/campaigns");
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/campaigns`);
   if (!res.ok) throw new Error("Failed to fetch campaigns");
   return res.json();
 }
